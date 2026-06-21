5dde9d5b941f76c5fad21f2cf43b5b7946bd1258
Tanushree0114 <tanushree012007@gmail.com>
2026-06-21 14:02:39 +0530

    test push

--- script.js
+++ script.js
@@ -209,3 +209,9 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const dropdown = document.querySelector(".dropdown");
+
+dropdown.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+});
